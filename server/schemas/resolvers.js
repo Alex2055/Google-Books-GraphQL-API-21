8fc6a946c7e155b2ed2f4781891cc2c44b92f171
Alex2055 <alexfromnashville@gmail.com>
2020-10-22 20:59:52 -0500

    server.js changed, typedefs, resolvers, auth added

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,65 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const resolvers = {
+Query: {
+    me: async (parent, args, context) => {
+        if(context.user) {
+        const userData = await User.findOne( { _id: context.user._id } )
+        .select('-__v -password')
+        return userData
+        }
+        throw new AuthenticationError('Not logged in');
+    }
+    
+},
+Mutation: {
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+      
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const correctPw = await user.isCorrectPassword(password);
+      
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const token = signToken(user);
+        return { token, user };
+      },
+    addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+        return { token, user };
+    },
+    saveBook: async (parent, {input}, context) => {
+        if (context.user) {
+            
+            const updateduser = await User.findByIdAndUpdate(
+            {_id: context.user._id},
+            { $addToSet: { savedBooks: input} },
+            { new: true})
+
+            return updateduser;
+    }
+    
+},
+removeBook: async (parent, params, context) => {
+    if (context.user) {
+        const deleteuser = await User.findByIdAndUpdate(
+            {_id: context.user._id},
+            { $pull: { savedBooks: {bookId: params.bookId}} },
+            { new: true}
+        )
+        return deleteuser;
+    }
+}
+    
+}
+}
+
+
+module.exports = resolvers;
